af558c3176c5e3454b97cad07d3070684ba20ebc
ZYSzys <17367077526@163.com>
2018-04-16 12:28:59 +0800

    ➕:one:

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,221 +1,221 @@
-var Elevator = function(options) {
-	"use strict";
+var Elevator = function (options) {
+  'use strict'
 
-	var body = null;
-	var that = this;
+  var body = null
+  var that = this
 
-	var startPosition = null;
-	var endPosition = 0;
-	var targetElement = null;
-	
-	var customDuration = false;
-	var duration = null;
+  var startPosition = null
+  var endPosition = 0
+  var targetElement = null
 
-	var verticalPadding = null;
+  var customDuration = false
+  var duration = null
 
-	var elevating = false;
+  var verticalPadding = null
 
-	var startTime = null;
+  var elevating = false
 
-	var animation = null;
+  var startTime = null
 
-	var mainAudio;
-	var endAudio;
-	var startCallback;
-	var endCallback;
+  var animation = null
 
-	//滑动算法 - http://robertpenner.com/easing/
-	function easeInOutQuad(t, b, c, d) {
-		t /= d / 2;
-		if(t < 1) return c / 2 * t * t + b;
-		t--;
-		return -c / 2 * (t * (t - 2) - 1) + b;
-	}
+  var mainAudio
+  var endAudio
+  var startCallback
+  var endCallback
 
-	//增加参数
-	function extendParameters(options, defaults) {
-		for(var option in defaults) {
-			var t = options[option] === undefined && typeof option !== 'function';
-			if(t) {
-				options[option] = defaults[option];
-			}
-		}
-		return options;
-	}
+  // 滑动算法 - http://robertpenner.com/easing/
+  function easeInOutQuad (t, b, c, d) {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return -c / 2 * (t * (t - 2) - 1) + b
+  }
 
-	//上升位移
-	function getVerticalOffset(element) {
-		var verticalOffset = 0;
-		while(element) {
-			verticalOffset += element.offsetTop || 0;
-			element = element.offsetParent;
-		}
+  // 增加参数
+  function extendParameters (options, defaults) {
+    for (var option in defaults) {
+      var t = options[option] === undefined && typeof option !== 'function'
+      if (t) {
+        options[option] = defaults[option]
+      }
+    }
+    return options
+  }
 
-		if(verticalPadding) {
-			verticalOffset = verticalOffset - verticalPadding;
-		}
-		return verticalOffset;
-	}
+  // 上升位移
+  function getVerticalOffset (element) {
+    var verticalOffset = 0
+    while (element) {
+      verticalOffset += element.offsetTop || 0
+      element = element.offsetParent
+    }
 
-	//动画循环
-	function animateLoop(time) {
-		if(!startTime) {
-			startTime = time;
-		}
-		var timeSoFar = time - startTime;
-		var easedPosition = easeInOutQuad(
-				timeSoFar,
-				startPosition,
-				endPosition - startPosition,
-				duration
-			);
-		window.scrollTo(0, easedPosition);
+    if (verticalPadding) {
+      verticalOffset = verticalOffset - verticalPadding
+    }
+    return verticalOffset
+  }
 
-		if(timeSoFar < duration) {
-			animation = requestAnimationFrame(animateLoop);
-		}else {
-			animationFinished();
-		}
-	}
+  // 动画循环
+  function animateLoop (time) {
+    if (!startTime) {
+      startTime = time
+    }
+    var timeSoFar = time - startTime
+    var easedPosition = easeInOutQuad(
+      timeSoFar,
+      startPosition,
+      endPosition - startPosition,
+      duration
+    )
+    window.scrollTo(0, easedPosition)
 
-	//main
-	this.elevate = function() {
-		if(elevating) {
-			return;
-		}
-		elevating = true;
-		startPosition = document.documentElement.scrollTop || body.scrollTop;
-		updateEndPosition();
-		if(!customDuration) {
-			duration = Math.abs(endPosition - startPosition) * 1.5;
-		}
-		requestAnimationFrame(animateLoop);
+    if (timeSoFar < duration) {
+      animation = requestAnimationFrame(animateLoop)
+    } else {
+      animationFinished()
+    }
+  }
 
-		if(mainAudio) {
-			mainAudio.play();
-		}
+  // main
+  this.elevate = function () {
+    if (elevating) {
+      return
+    }
+    elevating = true
+    startPosition = document.documentElement.scrollTop || body.scrollTop
+    updateEndPosition()
+    if (!customDuration) {
+      duration = Math.abs(endPosition - startPosition) * 1.5
+    }
+    requestAnimationFrame(animateLoop)
 
-		if(startCallback) {
-			startCallback();
-		}
-	}
+    if (mainAudio) {
+      mainAudio.play()
+    }
 
-	//重置初始位置
-	function resetPositions() {
-		startTime = null;
-		startPosition = null;
-		elevating = false;
-	}
+    if (startCallback) {
+      startCallback()
+    }
+  }
 
-	//更新结束位置
-	function updateEndPosition() {
-		if(targetElement) {
-			endPosition = getVerticalOffset(targetElement);
-		}
-	}
+  // 重置初始位置
+  function resetPositions () {
+    startTime = null
+    startPosition = null
+    elevating = false
+  }
 
-	//elevate动画完成
-	function animationFinished() {
-		resetPositions();
+  // 更新结束位置
+  function updateEndPosition () {
+    if (targetElement) {
+      endPosition = getVerticalOffset(targetElement)
+    }
+  }
 
-		if(mainAudio) {
-			mainAudio.pause();
-			mainAudio.currentTime = 0;
-		}
+  // elevate动画完成
+  function animationFinished () {
+    resetPositions()
 
-		if(endAudio) {
-			endAudio.play();
-		}
+    if (mainAudio) {
+      mainAudio.pause()
+      mainAudio.currentTime = 0
+    }
 
-		if(endCallback) {
-			endCallback();
-		}
-	}
+    if (endAudio) {
+      endAudio.play()
+    }
 
-	function onWindowBlur() {
-		if(elevating) {
-			cancelAnimationFrame(animation);
-			resetPositions();
+    if (endCallback) {
+      endCallback()
+    }
+  }
 
-			if(mainAudio) {
-				mainAudio.pause();
-				mainAudio.currentTime = 0;
-			}
+  function onWindowBlur () {
+    if (elevating) {
+      cancelAnimationFrame(animation)
+      resetPositions()
 
-			updateEndPosition();
-			window.scrollTo(0, endPosition);
-		}
-	}
+      if (mainAudio) {
+        mainAudio.pause()
+        mainAudio.currentTime = 0
+      }
 
-	//将元素与事件监听绑定
-	function bindElevateToElement(element) {
-		if(element.addEventListener) {
-			element.addEventListener("click", that.elevate, false);
-		}else {
-			element.attachEvent('onclick', function() {
-				updataEndPosition();
-				document.documentElement.scrollTop = endPosition;
-				document.body.scrollTop = endPosition;
-				window.scroll(0, endPosition);
-			});
-		}
-	}
+      updateEndPosition()
+      window.scrollTo(0, endPosition)
+    }
+  }
 
-	function init(_options) {
-		body = document.body;
+  // 将元素与事件监听绑定
+  function bindElevateToElement (element) {
+    if (element.addEventListener) {
+      element.addEventListener('click', that.elevate, false)
+    } else {
+      element.attachEvent('onclick', function () {
+        updataEndPosition()
+        document.documentElement.scrollTop = endPosition
+        document.body.scrollTop = endPosition
+        window.scroll(0, endPosition)
+      })
+    }
+  }
 
-		var defaults = {
-			duration: undefined,
-			mainAudio: false,
-			endAudio: false,
-			preloadAudio: true,
-			loopAudio: true,
-			startCallback: null,
-			endCallback: null
-		};
+  function init (_options) {
+    body = document.body
 
-		_options = extendParameters(_options, defaults);
+    var defaults = {
+      duration: undefined,
+      mainAudio: false,
+      endAudio: false,
+      preloadAudio: true,
+      loopAudio: true,
+      startCallback: null,
+      endCallback: null
+    }
 
-		if(_options.element) {
-			bindElevateToElement(_options.element);
-		}
+    _options = extendParameters(_options, defaults)
 
-		if(_options.duration) {
-			customDuration = true;
-			duration = _options.duration;
-		}
+    if (_options.element) {
+      bindElevateToElement(_options.element)
+    }
 
-		if(_options.targetElement) {
-			targetElement = _options.targetElement;
-		}
+    if (_options.duration) {
+      customDuration = true
+      duration = _options.duration
+    }
 
-		if(_options.verticalPadding) {
-			verticalPadding = _options.verticalPadding;
-		}
+    if (_options.targetElement) {
+      targetElement = _options.targetElement
+    }
 
-		window.addEventListener("blur", onWindowBlur, false);
+    if (_options.verticalPadding) {
+      verticalPadding = _options.verticalPadding
+    }
 
-		if(_options.mainAudio) {
-			mainAudio = new Audio(_options.mainAudio);
-			mainAudio.setAttribute("preload", _options.preloadAudio);
-			mainAudio.setAttribute("loop", _options.loopAudio);
-		}
+    window.addEventListener('blur', onWindowBlur, false)
 
-		if(_options.endAudio) {
-			endAudio = new Audio(_options.endAudio);
-			endAudio.setAttribute("preload", "true");
-		}
+    if (_options.mainAudio) {
+      mainAudio = new Audio(_options.mainAudio)
+      mainAudio.setAttribute('preload', _options.preloadAudio)
+      mainAudio.setAttribute('loop', _options.loopAudio)
+    }
 
-		if(_options.endCallback) {
-			endCallback = _options.endCallback;
-		}
-		if(_options.startCallback) {
-			startCallback._options.startCallback;
-		}
-	}
-	init(options);
-};
+    if (_options.endAudio) {
+      endAudio = new Audio(_options.endAudio)
+      endAudio.setAttribute('preload', 'true')
+    }
 
-if(typeof module !== 'undefined' && module.exports) {
-	module.exports = Elevator;
+    if (_options.endCallback) {
+      endCallback = _options.endCallback
+    }
+    if (_options.startCallback) {
+      startCallback._options.startCallback
+    }
+  }
+  init(options)
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Elevator
 }
